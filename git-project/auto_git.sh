5b3bbc26b370570315e184c1f5c69e99b883dfb6
Md Akramul Islam <md.akramul.islam@sap.com>
2025-02-27 14:57:40 +0100

    Auto commit: Thu Feb 27 14:57:40 CET 2025

--- git-project/auto_git.sh
+++ git-project/auto_git.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Change to the directory of your Git repository
-cd /Users/D073963/mygithub/learn-cloud-ops-main || exit
+cd /Users/D073963/mygithub/learn-cloud-ops-main/git-project || exit
 
-# Add all changes
+git checkout main
+git pull --rebase origin main || git rebase --abort
+
 git add .
-
-# Commit with a timestamp message
-commit_message="Auto commit: $(date +'%Y-%m-%d %H:%M:%S')"
-git commit -m "$commit_message"
-
-# Push changes to the remote repository
-git push origin main  # Change 'main' to your branch if needed
-
-echo "Git changes pushed successfully!"
+git commit -m "Auto commit: $(date)"
+git push origin main
