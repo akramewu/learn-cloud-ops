078481a252963780f5366255cc4ea6726266c8a6
Md Akramul Islam <mdakramulislam@Mac.lan>
2025-11-15 09:10:59 +0000

    backend.tf added

--- 12-weeks-terraform-aws/week2-vpc-networking/backend.tf
+++ 12-weeks-terraform-aws/week2-vpc-networking/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "akramul-terraform-state-eu-west-2"  # <-- your S3 bucket name
-    key    = "week1/terraform.tfstate"            # path inside the bucket
+    key    = "week2/terraform.tfstate"            # path inside the bucket
     region = "eu-west-2"                          # same as bucket region
   }
 }
