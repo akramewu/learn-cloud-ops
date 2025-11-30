3f84c88431e3eac4406f8c848e16668cb5b378dd
Md Akramul Islam <mdakramulislam@Mac.lan>
2025-11-30 22:47:37 +0000

    Clean repo and remove large files from Git history

--- 12-weeks-terraform-aws/week3-alb-autoscaling/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "akramul-terraform-state-eu-west-2"  # <-- your S3 bucket name
-    key    = "week3/terraform.tfstate"            # path inside the bucket
-    region = "eu-west-2"                          # same as bucket region
-  }
-}
--- 12-weeks-terraform-aws/week3-alb-autoscaling/providers.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = "eu-west-2" #london to match week 1
-}
