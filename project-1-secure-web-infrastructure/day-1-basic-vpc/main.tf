927cc624546211fc4388cf14aec5b7c0f40c664b
Md Akramul Islam <mdakramulislam@Mac.lan>
2025-12-07 14:09:20 +0000

    project-1-secure-web-infrastructure

--- project-1-secure-web-infrastructure/day-1-basic-vpc/main.tf
+++ project-1-secure-web-infrastructure/day-1-basic-vpc/main.tf
@@ -38,6 +38,11 @@ resource "aws_route_table" "public_rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+    tags = {
+        Name    = "${var.project}-public-rt"
+        Project = var.project
+    }
 }
 
 resource "aws_route_table_association" "public_rta" {
